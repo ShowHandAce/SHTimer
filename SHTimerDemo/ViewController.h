ec74e3f8cf77634da962604c8d8f48f5d1b88e3d
chengqiushi <qiushi0927@163.com>
2017-11-20 10:33:16 +0800

    Initial Commit

---
+++ SHTimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHTimerDemo
+//
+//  Created by 程秋实 on 2017/11/20.
+//  Copyright © 2017年 Criss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SHTimerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SHTimerDemo
+//
+//  Created by 程秋实 on 2017/11/20.
+//  Copyright © 2017年 Criss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
